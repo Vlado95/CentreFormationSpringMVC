7c6c6e20b8846cf0a0a9aca67d61baae0a5dd98a
Jean Vladimir TWAHIRWA <tvradmir@gmail.com>
2017-03-15 00:46:07 +0100

    Revert "regi sec"
    This reverts commit d096e684b3eb018d1387c11e56f7ac1b090fa136.

--- src/main/java/com/cefisi/controllers/RegistrationController.java
+++ src/main/java/com/cefisi/controllers/RegistrationController.java
@@ -7,8 +7,6 @@ package com.cefisi.controllers;
 
 import com.cefisi.modeles.Adresse;
 import com.cefisi.modeles.Personne;
-import com.cefisi.registration.user.OnRegistrationCompleteEvent;
-import com.cefisi.service.IUserService;
 import com.cefisi.validation.UserValidation;
 import java.sql.SQLException;
 import java.util.HashSet;
@@ -20,20 +18,16 @@ import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -41,12 +35,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class RegistrationController {
-    
-    @Autowired
-    private ApplicationEventPublisher eventPublisher;
 
-    @Autowired
-    private IUserService userService;
 //    UserValidation userValidator = new UserValidation();
 //
 //    @Autowired
@@ -56,90 +45,61 @@ public class RegistrationController {
     @PersistenceContext
     private EntityManager entityManager;
     
-//    @RequestMapping(value = "/reg-new-user", method = RequestMethod.GET)
-//    public String askNewUser(ModelMap map, ServletRequest request) {
-//
-//        final HttpServletRequest req = (HttpServletRequest) request;
-//        HttpServletRequest requesti = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-//        Personne user = new Personne();
-//        
-//       /* map.put("url2", requesti.getRequestURL());
-//        map.put("url", req.getServletPath());*/
-//        map.put("user", user);
-//        map.put("action", "Enregistre");
-//        map.put("titre", "Creer un nouveau utilisateur");
-//        return "registrationForm";
-//    }
-//
-//    @Transactional
-//    @RequestMapping(value = "/reg-new-user", method = RequestMethod.POST)
-//    public String doNewUser(@Valid @ModelAttribute("user") Personne user,
-//            BindingResult result, ModelMap map, SessionStatus status
-//    ) throws SQLException {
-//
-//        UserValidation userValidator = new UserValidation();
-//        userValidator.validate(user, result);
-//        
-//        Adresse adresse = new Adresse();
-//        adresse.setRue(user.getRue());
-//        adresse.setVille(user.getVille());
-//        adresse.setCodePostal(user.getCodePostal());
-//        adresse.setMobile(user.getMobile());
-//        adresse.setFixe(user.getFixe());
-//        adresse.setPays(user.getPays());
-//        user.setProfil(Boolean.FALSE);
-//        Set<Personne> newUsers = new HashSet<Personne>();
-//        newUsers.add(user);
-//        adresse.setPersonnes(newUsers);
-//        
-//        map.put("action", "Enregistre");
-//        map.put("titre", "Creer un nouveau utilisateur");
-//        map.put("newpersonne", user);
-//        if (result.hasErrors()) {
-//            //if validator failed
-//            return "registrationForm";
-//        } else {
-//            status.setComplete();
-//            System.out.println("userId " + user.getIdPersonne());
-//            System.out.println("user " + user.getNom());
-//            System.out.println("user " + user.getRue());
-//            System.out.println("adresse " + adresse.getVille());
-//            entityManager.persist(adresse);
-//            entityManager.flush();
-//            System.out.println("ok");
-//            //form success
-//            return "registrationSuccess";
-//        }
-//        // return "registrationSuccess";
-//    }
-//    
     @RequestMapping(value = "/reg-new-user", method = RequestMethod.GET)
-    public String showRegistrationForm(final HttpServletRequest request, final Model model) {
-        //LOGGER.debug("Rendering registration page.");
-        final Personne accountDto = new Personne();
-        model.addAttribute("user", accountDto);
+    public String askNewUser(ModelMap map, ServletRequest request) {
+
+        final HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletRequest requesti = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        Personne user = new Personne();
+        
+       /* map.put("url2", requesti.getRequestURL());
+        map.put("url", req.getServletPath());*/
+        map.put("user", user);
+        map.put("action", "Enregistre");
+        map.put("titre", "Creer un nouveau utilisateur");
         return "registrationForm";
     }
-    
-    
-        @RequestMapping(value = "/reg-new-user", method = RequestMethod.POST)
-    public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid final Personne userDto,
-             final HttpServletRequest request, final Errors errors) {
-        //LOGGER.debug("Registering user account with information: {}", userDto);
 
-        final Personne registered = userService.registerNewUserAccount(userDto);
-        if (registered == null) {
-            // result.rejectValue("email", "message.regError");
-            return new ModelAndView("registrationForm", "user", userDto);
+    @Transactional
+    @RequestMapping(value = "/reg-new-user", method = RequestMethod.POST)
+    public String doNewUser(@Valid @ModelAttribute("user") Personne user,
+            BindingResult result, ModelMap map, SessionStatus status
+    ) throws SQLException {
+
+        UserValidation userValidator = new UserValidation();
+        userValidator.validate(user, result);
+        
+        Adresse adresse = new Adresse();
+        adresse.setRue(user.getRue());
+        adresse.setVille(user.getVille());
+        adresse.setCodePostal(user.getCodePostal());
+        adresse.setMobile(user.getMobile());
+        adresse.setFixe(user.getFixe());
+        adresse.setPays(user.getPays());
+        user.setProfil(Boolean.FALSE);
+        Set<Personne> newUsers = new HashSet<Personne>();
+        newUsers.add(user);
+        adresse.setPersonnes(newUsers);
+        
+        map.put("action", "Enregistre");
+        map.put("titre", "Creer un nouveau utilisateur");
+        map.put("newpersonne", user);
+        if (result.hasErrors()) {
+            //if validator failed
+            return "registrationForm";
+        } else {
+            status.setComplete();
+            System.out.println("userId " + user.getIdPersonne());
+            System.out.println("user " + user.getNom());
+            System.out.println("user " + user.getRue());
+            System.out.println("adresse " + adresse.getVille());
+            entityManager.persist(adresse);
+            entityManager.flush();
+            System.out.println("ok");
+            //form success
+            return "registrationSuccess";
         }
-        try {
-            final String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
-            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), appUrl));
-        } catch (final Exception ex) {
-         //   LOGGER.warn("Unable to register user", ex);
-            return new ModelAndView("emailError", "user", userDto);
-        }
-        return new ModelAndView("registrationSuccess", "user", userDto);
+        // return "registrationSuccess";
     }
 
 }
